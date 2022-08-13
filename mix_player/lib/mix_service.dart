b695e6eca93fae1e5c5231d7077ccc34dedac599
ApisitKaewsasan <ApisitKaewsasan@hotmail.com>
2022-08-13 23:23:19 +0700

    update

--- mix_player/lib/mix_service.dart
+++ mix_player/lib/mix_service.dart
@@ -41,9 +41,7 @@ class MixService {
 
       var fileName = "${pathCache.isNotEmpty?pathCache:directory.path}/${mixItem.fileName}.${mixItem.extension.toLowerCase()}";
       if (File(mixItem.request.first).existsSync()) {
-        File(fileName).exists().then((value){
-          File(fileName).delete();
-        });
+
         if (mixItem.request.length>0 && p.extension(mixItem.request.first).split(".")[1] ==
             mixItem.extension.toLowerCase()) {
           onError.call(
